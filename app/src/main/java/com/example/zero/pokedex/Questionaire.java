31c46db18b90b6c3409c3b81b56e3766b4c73f31
yamas2019 <32025539+yamas2019@users.noreply.github.com>
2017-10-16 18:57:00 -0400

    Alex Yam - Monday, October 16 - Gave users the option to enter in their answers for the questionnaire and made questionnaire activity screen look "cleaner".

--- app/src/main/java/com/example/zero/pokedex/Questionaire.java
+++ app/src/main/java/com/example/zero/pokedex/Questionaire.java
@@ -5,13 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import static com.example.zero.pokedex.R.id.textView2;
 
 public class Questionaire extends AppCompatActivity {
+
+    String Color, Movie, Animal, Book;
+
+    EditText editColor;
+    EditText editMovie;
+    EditText editAnimal;
+    EditText editBook;
+
+    Button btnSubmitInfo;
+
+
     public Button btn_submitInfo;
     public Globals global;
+
     public void submitInfo()
     {
         btn_submitInfo = (Button)findViewById(R.id.btnSubmitInfo   );
@@ -33,6 +46,13 @@ public class Questionaire extends AppCompatActivity {
         global = (Globals)getApplicationContext();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionaire);
+
+        editColor = (EditText) findViewById(R.id.editColor);
+        editMovie = (EditText) findViewById(R.id.editMovie);
+        editAnimal = (EditText) findViewById(R.id.editAnimal);
+        editBook = (EditText) findViewById(R.id.editBook);
+
+        //btnSubmitInfo = (Button) findViewById((R.id.btnSubmitInfo));
         submitInfo();
 
     }

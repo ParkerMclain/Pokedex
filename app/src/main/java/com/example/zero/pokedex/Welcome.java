bab1c17cbfec9dd1a5bfa2d1d24b07fef9c6b43b
Mclainpw <mclainpw@vcu.edu>
2017-11-17 08:23:05 -0500

    Update to a couple files to make the merge work

--- app/src/main/java/com/example/zero/pokedex/Welcome.java
+++ app/src/main/java/com/example/zero/pokedex/Welcome.java
@@ -7,29 +7,30 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class Welcome extends AppCompatActivity {
-    private FirebaseAuth mAuth;
+    private FirebaseAuth  mauth;
 
     public Button btnUnlock;
-    public void initialize(){
-        btnUnlock = (Button)findViewById(R.id.btnStart);
+
+    public void initialize() {
         btnUnlock.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent LoginFirebase = new Intent(Welcome.this, com.example.zero.pokedex.LoginFirebase.class);
-                startActivity(LoginFirebase);
+            public void onClick(View viewer) {
+                Intent loginFirebase = new Intent(Welcome.this, loginFirebase.class);
+                startActivity(loginFirebase);
 
             }
-        });}
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mAuth = FirebaseAuth.getInstance();
+        mauth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        mAuth.signOut();
+        mauth.signOut();
+        btnUnlock = (Button)findViewById(R.id.btnStart);
         initialize();
 
 
